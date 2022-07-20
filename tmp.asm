b066ff8f942f70adf1a5d656b0afa5b84c5dd09c
Federico <96267363+dalps@users.noreply.github.com>
2022-07-20 16:42:21 +0200

    Exit video mode on keypress

--- tmp.asm
+++ tmp.asm
@@ -16,3 +16,18 @@ ClearScreen:
 
     loop ClearScreen
 
+getKeyStroke:
+    xor ax, ax
+    int 16h ; read a keystroke, ASCII char is stored in AL
+
+    cmp al, 0x71 ; 'q'
+    je exitVideoMode
+    cmp al, 0x51 ; 'Q'
+    je exitVideoMode
+
+    jmp getKeyStroke
+
+exitVideoMode:
+    mov ax, 0x0003
+    int 0x10
+    ret ; setting the mode is not enough, you gotta return to the shell!
