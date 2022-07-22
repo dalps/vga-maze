8aeb98f6f7533b6f200320d86e3c370412314370
Federico <96267363+dalps@users.noreply.github.com>
2022-07-22 10:03:04 +0200

    Indent loops + tidy up

--- tmp.asm
+++ tmp.asm
@@ -1,6 +1,5 @@
 org 100h ; offset in the current segment
 
-section .data
 SCREEN_WIDTH    equ 320
 SCREEN_HEIGHT   equ 200
 SCREEN_PIXELS   equ SCREEN_WIDTH*SCREEN_HEIGHT
@@ -9,8 +8,8 @@ MAZE_ROWS       equ 25
 CELL_SIZE       equ 8
 
 section .data
-Visited         times 1000 db 0
-Neighbors       dw -1, 40, 1, -40 ; needs to be a list of initalizers!
+Visited         times MAZE_ROWS*MAZE_COLS db 0
+Neighbors       dw -1, MAZE_COLS, 1, -MAZE_COLS
 
 section .bss
 Color           resb 1 ; pointer to variable Color in data section
@@ -18,61 +17,50 @@ Seed            resw 1
  
 section .text
 main:
+    mov ax, 0xA000
+    mov es, ax
+
     mov ax, 0x0013
     int 0x10
 
-    xor ax, ax
+    mov ax, 0
     int 0x1a
-    ; number of ticks since midnight is stored in cx:dx (we're just taking the lower 2 bytes here)
-    mov word [Seed], dx
+    mov word [Seed], dx ; number of ticks since midnight is stored in cx:dx (we're just taking the lower 2 bytes here)
 
-    mov ax, 0xA000
-    mov es, ax
-    
     mov byte [Color], 0xf   
     call ClearScreen
-
-    mov word [Color], 0x7
-    call DrawGrid
 
     call VisitBorder
 
     push 41
-    call DFS
+    call DrawMaze
+    
+    getKeyStroke:
+        xor ax, ax
+        int 16h ; read a keystroke, ASCII char is stored in AL
 
-getKeyStroke:
-    xor ax, ax
-    int 16h ; read a keystroke, ASCII char is stored in AL
+        cmp al, 0x71 ; 'q'
+        je exitVideoMode
+        cmp al, 0x51 ; 'Q'
+        je exitVideoMode
 
-    cmp al, 0x71 ; 'q'
-    je exitVideoMode
-    cmp al, 0x51 ; 'Q'
-    je exitVideoMode
+        jmp getKeyStroke
 
-    jmp getKeyStroke
-
-exitVideoMode:
-    mov ax, 0x0003
-    int 0x10
-    ret ; setting the mode is not enough, you gotta return to the shell!
+    exitVideoMode:
+        mov ax, 0x0003
+        int 0x10
+        ret ; setting the mode is not enough, you gotta return to the shell!
 
 ; ---------------------------------------------------------------------------------------------------------------------
 ; Procedures
 ;
 
-; linear index of current cell is at [bp+4]
-DFS:
+; linear index of current cell is at [bp+6]
+DrawMaze:
     push cx ; save caller's iteration number
     push bp
     mov bp, sp
-
     sub sp, 2 ; space for next neighbor's index
-
-    mov byte [Color], 0xf
-    push 8
-    push 7
-    push 1
-    call HLine
 
     ; visit the current cell
     mov bx, word [bp+6]
@@ -84,119 +72,103 @@ DFS:
 
     mov cx, 4
 
-VisitNeighbors:
-    ; compute the linear offset of the neighbor
-    mov bx, word [bp-2]
-    add bx, bx
-    mov ax, word [bp+6]
-    add ax, word [Neighbors + bx]
-    mov bx, ax
+    VisitNeighbors:
+        ; compute the linear offset of the neighbor
+        mov bx, word [bp-2]
+        add bx, bx
+        mov ax, word [bp+6]
+        add ax, word [Neighbors + bx]
+        mov bx, ax
 
-    cmp byte [Visited + bx], 1
-    je Continue ; neighbor has been visited and must be skipped
+        cmp byte [Visited + bx], 1
+        je Continue ; neighbor has been visited and must be skipped
 
-    ; get the coordinates of the neighbor's cell top-left corner
-    mov ax, bx
-    mov dl, MAZE_COLS
-    div dl ; neighbor_x := AH; neighbor_y := AL
+        ; get the coordinates of the neighbor's cell top-left corner
+        mov ax, bx
+        mov dl, MAZE_COLS
+        div dl ; neighbor_x := AH; neighbor_y := AL
 
-    ; convert to screen coordinates: vga_x := neighbor_x*8; vga_y := neighbor_y*8
-    xor dx, dx
-    mov dl, ah
-    mov di, dx ; save neighbor_x
-    mov dl, al
-    mov si, dx ; save neighbor_y
+        ; convert to screen coordinates: vga_x := neighbor_x*8; vga_y := neighbor_y*8
+        xor dx, dx
+        mov dl, ah
+        mov di, dx ; save neighbor_x
+        mov dl, al
+        mov si, dx ; save neighbor_y
 
-    mov ax, di
-    mov dl, CELL_SIZE
-    imul dl
-    mov di, ax
-    
-    mov ax, si
-    mov dl, CELL_SIZE
-    imul dl
-    mov si, ax
+        mov ax, di
+        mov dl, CELL_SIZE
+        imul dl
+        mov di, ax
+        
+        mov ax, si
+        mov dl, CELL_SIZE
+        imul dl
+        mov si, ax
 
-    ; destroy the wall separating current cell and visited neighbor
-    cmp word [bp-2], 0
-    je DestroyLeftWall
-    cmp word [bp-2], 1
-    je DestroyBottomWall
-    cmp word [bp-2], 2
-    je DestroyRightWall
-    cmp word [bp-2], 3
-    je DestroyTopWall
+        ; destroy the wall separating current cell and visited neighbor
+        cmp word [bp-2], 0
+        je DestroyLeftWall
+        cmp word [bp-2], 1
+        je DestroyBottomWall
+        cmp word [bp-2], 2
+        je DestroyRightWall
+        cmp word [bp-2], 3
+        je DestroyTopWall
 
-    mov byte [Color], 0xf
+        mov byte [Color], 0x1
 
-DestroyLeftWall:
-    ; VLine(x, y1, y1+8)
-    push cx
-    push bx
-    add si, 8
-    push si
-    push si
-    push di
-    call VLine
-    pop bx
-    pop cx
-    jmp Recursion
-DestroyBottomWall:
-    ; HLine(x, x+8, y+8)
-    push cx
-    push bx
-    push si
-    add si, 8
-    push si
-    add di, 8
-    push di
-    call HLine
-    pop bx
-    pop cx
-    jmp Recursion
-DestroyRightWall:
-    ; VLine(x+8, y, y+8)
-    push cx
-    push bx
-    add si, 8
-    push si
-    push si
-    add di, 8
-    push di
-    call VLine
-    pop bx
-    pop cx
-    jmp Recursion
-DestroyTopWall:
-    ; HLine(x, x+8, y)
-    push cx
-    push bx
-    push si
-    add di, 8
-    push di
-    push di
-    call HLine
-    pop bx
-    pop cx
-    jmp Recursion
+        DestroyLeftWall:
+            ; VLine(x, y1, y1+8)
+            add si, 8
+            push si ; y2
+            push si ; y1
+            push di ; x
+            call VLine
+            jmp Recursion
+        DestroyBottomWall:
+            ; HLine(x, x+8, y+8)
+            push si
+            add si, 8
+            push si
+            add di, 8
+            push di
+            call HLine
+            jmp Recursion
+        DestroyRightWall:
+            ; VLine(x+8, y, y+8)
+            add si, 8
+            push si
+            push si
+            add di, 8
+            push di
+            call VLine
+            jmp Recursion
+        DestroyTopWall:
+            ; HLine(x, x+8, y)
+            push si
+            add di, 8
+            push di
+            push di
+            call HLine
+            jmp Recursion
 
-Recursion:
-    push bx
-    call DFS
+        Recursion:
+            push bx
+            call DrawMaze
 
-Continue:
-    ; next <- (next + 1) % 4
-    mov ax, word [bp-2]
-    inc ax
-    mov dl, 4
-    div dl
-    mov al, ah
-    mov ah, 0
-    mov word [bp-2], ax
+    Continue:
+        ; next <- (next + 1) % 4
+        mov ax, word [bp-2]
+        inc ax
+        mov dl, 4
+        div dl
+        mov al, ah
+        mov ah, 0
+        mov word [bp-2], ax
 
-    dec cx
-    cmp cx, 0
-    je VisitNeighbors
+        add cx, -1
+        cmp cx, 0
+        je VisitNeighbors
 
     mov sp, bp
     pop bp
@@ -211,20 +183,20 @@ VisitBorder:
     xor bx, bx
     mov cx, MAZE_COLS
 
-VisitHorzBorder:
-    mov byte [Visited + bx], 1
-    mov byte [Visited + bx + MAZE_COLS*MAZE_ROWS-MAZE_COLS], 1 ; wasted a lot of time cause i subtracted rows instead of columns, thus writing out of bounds
-    inc bx
-    loop VisitHorzBorder
+    VisitHorzBorder:
+        mov byte [Visited + bx], 1
+        mov byte [Visited + bx + MAZE_COLS*MAZE_ROWS-MAZE_COLS], 1
+        inc bx
+        loop VisitHorzBorder
 
     xor bx, bx
     mov cx, MAZE_ROWS
 
-VisitVertBorder:
-    mov byte [Visited + bx-1], 1
-    mov byte [Visited + bx], 1
-    add bx, MAZE_COLS
-    loop VisitVertBorder
+    VisitVertBorder:
+        mov byte [Visited + bx-1], 1
+        mov byte [Visited + bx], 1
+        add bx, MAZE_COLS
+        loop VisitVertBorder
 
     pop bp
     ret
@@ -235,6 +207,11 @@ MiniRNG:
     push bp
     mov bp, sp
 
+    ; seed := time()
+    ; seed ^= seed << 7
+    ; seed ^= seed >> 9
+    ; seed ^= seed << 8
+    ; -> seed & 3
     mov ax, word [Seed]
     mov dx, ax
     shl ax, 7
@@ -263,139 +240,130 @@ ClearScreen:
     xor bx, bx
     mov al, [Color]
 
-ClearScreenLoop:
-    mov [es:bx], al
-    inc bx
-
-    loop ClearScreenLoop
+    ClearScreenLoop:
+        mov [es:bx], al
+        inc bx
+        loop ClearScreenLoop
 
     pop bp
     ret
 
 
-; x is at bp+4
-; y is at bp+6
-; Color is set by the caller
-SetPixel:
-    push bp
-    mov bp, sp
 
-    ; check if parameters are legal offsets
-    cmp word [bp+4], 0
-    jl EndSetPixel
-    cmp word [bp+4], SCREEN_WIDTH
-    jge EndSetPixel
-    cmp word [bp+6], 0
-    jl EndSetPixel
-    cmp word [bp+6], SCREEN_HEIGHT
-    jge EndSetPixel
-
-    ; offset := 320*y + x
-    mov ax, SCREEN_WIDTH
-    imul word [bp+6]
-    add ax, word [bp+4]
-    mov bx, ax
-
-    mov al, [Color]
-    mov [es:bx], al
-
-EndSetPixel:
-    pop bp
-    ret 4
-
-
-; x1 is at bp+4
-; x2 is at bp+6
-; y is at bp+8
+; x1 is at bp+14
+; x2 is at bp+16
+; y is at bp+18
 ; Color is set by the caller
 HLine:
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
     push bp
     mov bp, sp
 
     ; x2 < SCREEN_WIDTH
-    cmp word [bp+6], SCREEN_WIDTH
+    cmp word [bp+16], SCREEN_WIDTH
     jge EndHLine
     ; x1 <= x2
-    mov ax, word [bp+6]
-    cmp word [bp+4], ax
+    mov ax, word [bp+16]
+    cmp word [bp+14], ax
     jg EndHLine
     ; 0 <= x1
-    cmp word [bp+4], 0
+    cmp word [bp+14], 0
     jl EndHLine
     ; 0 <= y < SCREEN_HEIGHT
-    cmp word [bp+8], 0
+    cmp word [bp+18], 0
     jl EndHLine
-    cmp word [bp+8], SCREEN_HEIGHT
+    cmp word [bp+18], SCREEN_HEIGHT
     jge EndHLine
 
     ; line start at offset := 320*y + x1
     mov ax, SCREEN_WIDTH
-    imul word [bp+8]
-    add ax, word [bp+4]
+    imul word [bp+18]
+    add ax, word [bp+14]
     mov bx, ax
 
     ; draw x2 - x1 + 1 pixels
-    mov cx, word [bp+6]
-    sub cx, word [bp+4]
+    mov cx, word [bp+16]
+    sub cx, word [bp+14]
     inc cx
 
     mov al, [Color]
 
-HLineLoop:
-    mov [es:bx], al
-    inc bx
-    loop HLineLoop
+    HLineLoop:
+        mov [es:bx], al
+        inc bx
+        loop HLineLoop
 
-EndHLine:
-    pop bp
-    ret 6
+    EndHLine:
+        pop bp
+        pop di
+        pop dx
+        pop cx
+        pop bx
+        pop ax
+        ret 6
 
 
-; x is at bp+4
-; y1 is at bp+6
-; y2 is at bp+8
+
+; x is at bp+14
+; y1 is at bp+16
+; y2 is at bp+18
 ; Color is set by the caller
 VLine:
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
     push bp
     mov bp, sp
 
     ; y2 < SCREEN_HEIGHT
-    cmp word [bp+8], SCREEN_HEIGHT
+    cmp word [bp+18], SCREEN_HEIGHT
     jge EndVLine
     ; y1 <= y2
-    mov ax, word [bp+8]
-    cmp word [bp+6], ax
+    mov ax, word [bp+18]
+    cmp word [bp+16], ax
     jg EndVLine
     ; 0 <= y1
-    cmp word [bp+6], 0
+    cmp word [bp+16], 0
     jl EndVLine
     ; 0 <= x < SCREEN_WIDTH
-    cmp word [bp+4], 0
+    cmp word [bp+14], 0
     jl EndVLine
-    cmp word [bp+4], SCREEN_WIDTH ; YOU LEFT 200 IDIOT! DON'T COPY PASTE!!! WASTED SLEEP BECAUSE OF THIS
+    cmp word [bp+14], SCREEN_WIDTH
     jge EndVLine
 
     ; line start at offset := 320*y1 + x
     mov ax, SCREEN_WIDTH
-    imul word [bp+6]
-    add ax, word [bp+4]
+    imul word [bp+16]
+    add ax, word [bp+14]
     mov bx, ax
 
     ; draw y2 - y1 + 1 pixels
-    mov cx, word [bp+8]
-    sub cx, word [bp+6]
+    mov cx, word [bp+18]
+    sub cx, word [bp+16]
     inc cx
 
     mov al, [Color]
 
-VLineLoop:
-    mov [es:bx], al
-    add bx, SCREEN_WIDTH ; step between raster lines (320 bytes apart)
-    loop VLineLoop
+    VLineLoop:
+        mov [es:bx], al
+        add bx, SCREEN_WIDTH ; step between raster lines (320 bytes apart)
+        loop VLineLoop
 
-EndVLine:
-    pop bp
-    ret 6
+    EndVLine:
+        pop bp
+        pop di
+        pop dx
+        pop cx
+        pop bx
+        pop ax
+        ret 6
+
 
 
 ; Color is set by the caller
@@ -405,37 +373,27 @@ DrawGrid:
 
     xor dx, dx
     mov cx, word MAZE_ROWS 
+    RowLoop:
+        push dx ; y
+        push word SCREEN_WIDTH-1 ; x2
+        push word 0 ; x1
+        call HLine
 
-RowLoop:
-    push cx ; we're saving registers in the caller's frame here (HLine overrides them)
-    push dx
-    push dx ; y
-    push word SCREEN_WIDTH-1 ; x2
-    push word 0 ; x1
-    call HLine
+        add dx, CELL_SIZE
+        loop RowLoop
 
-    pop dx
-    add dx, CELL_SIZE
-    pop cx
-
-    loop RowLoop
 
     xor dx, dx
     mov cx, word MAZE_COLS
+    ColLoop:
+        push word SCREEN_HEIGHT-1 ; y2
+        push word 0; y1
+        push dx; x
+        call VLine
 
-ColLoop:
-    push cx
-    push dx
-    push word SCREEN_HEIGHT-1 ; y2
-    push word 0; y1
-    push dx; x
-    call VLine
+        add dx, CELL_SIZE
+        loop ColLoop
 
-    pop dx
-    add dx, CELL_SIZE
-    pop cx
-
-    loop ColLoop
 
     pop bp
     ret
