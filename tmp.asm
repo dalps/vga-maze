ad3a2863f0fa398fad32e1d9d3e9d3d1e9ba13f8
Federico <96267363+dalps@users.noreply.github.com>
2022-07-23 16:48:38 +0200

    Document functions + tidy up

--- tmp.asm
+++ tmp.asm
@@ -1,70 +1,87 @@
-org 100h ; offset in the current segment
+org 100h ; code offset in the current segment
+global main
 
 SCREEN_WIDTH    equ 320
 SCREEN_HEIGHT   equ 200
 SCREEN_PIXELS   equ SCREEN_WIDTH*SCREEN_HEIGHT
-MAZE_COLS       equ 40
-MAZE_ROWS       equ 25
 CELL_SIZE       equ 8
-DELAY           equ 50000 ; 0-65535
-VISITED_COLOR   equ 0xa
-DISCOVERED_COLOR equ 0xc
+MAZE_COLS       equ SCREEN_WIDTH/CELL_SIZE
+MAZE_ROWS       equ SCREEN_HEIGHT/CELL_SIZE
+WALK_DELAY           equ 50000 ; time to wait before visiting neighbors; can range from 0 to65535
+
+START_COLOR         equ 0x36
+WALL_COLOR          equ 0x08
+BORDER_COLOR        equ 0x08
+BACKGROUND_COLOR    equ 0x0f
+VISITED_COLOR       equ 0x42
+DISCOVERED_COLOR    equ 0x34
 
 section .data
-Visited         times MAZE_ROWS*MAZE_COLS db 0
-Neighbors       dw -1, MAZE_COLS, 1, -MAZE_COLS
+Visited         times MAZE_ROWS*MAZE_COLS db 0  ; keeps track of visited cells
+Neighbors       dw -1, MAZE_COLS, 1, -MAZE_COLS ; relative offsets in the maze's grid
 
 section .bss
-Color           resb 1 ; pointer to variable Color in data section
+Color           resb 1 ; each pixel is a byte representing the displayed color
 Seed            resw 1
  
 section .text
 main:
+    ; load ES with the video screen segment's base address
     mov ax, 0xA000
     mov es, ax
 
+    ; enter video mode (AH = 0) in graphical mode (AL = 0x13)
     mov ax, 0x0013
     int 0x10
 
-    mov ax, 0
-    int 0x1a
-    mov word [Seed], dx ; number of ticks since midnight is stored in cx:dx (we're just taking the lower 2 bytes here)
+    ; get system time (AH = 0) to initialize the RNG state
+    xor ax, ax
+    int 0x1a            ; BIOS stores number of ticks since midnight in CX:DX
+    mov word [Seed], dx ; the lower two bytes will suffice
 
-    mov byte [Color], 0xf   
-    call ClearScreen
-
-    mov byte [Color], 0x7 
+    ; flood the screen with a solid color, then draw a grid pattern
+    mov byte [Color], BACKGROUND_COLOR   
+    call FillScreen
     call DrawWalls
 
+    ; pick a random cell from where to begin walking the pattern
     push MAZE_ROWS*MAZE_COLS-MAZE_COLS-2    ; cell at (24;38)
     push MAZE_ROWS+1                        ; cell at (1;1)
-    call MiniRNG ; pick a starting cell randomly from the interval [41;958]
+    call MiniRNG 
     push ax
     call DrawMaze
     
-    getKeyStroke:
+    ; wait until user enters either 'q' or 'Q' to exit the program
+    GetQuitChar:
+        ; read a keystroke from the keyboard (AH = 0x0)
         xor ax, ax
-        int 16h ; read a keystroke, ASCII char is stored in AL
+        int 16h ; BIOS stores ASCII character in AL; waits if buffer is empty
 
-        cmp al, 0x71 ; 'q'
-        je exitVideoMode
-        cmp al, 0x51 ; 'Q'
-        je exitVideoMode
+        cmp al, 'q'
+        je ResetMode
+        cmp al, 'Q'
+        je ResetMode
+        jmp GetQuitChar
 
-        jmp getKeyStroke
-
-    exitVideoMode:
+    ResetMode:
+        ; set the video mode back to text mode (AL = 0x03)
         mov ax, 0x0003
         int 0x10
-        ret ; setting the mode is not enough, you gotta return to the shell!
 
-; ---------------------------------------------------------------------------------------------------------------------
-; Procedures
+    ret
+
+
+; ----------------------------------------------------------------------------------------------------------------------
+; Fills the inside of a maze cell of coordinates (x,y) with a solid color.
+; Caller may specify the desired color in the global variable Color.
 ;
-
-; fills the inside of a cell with a solid color (set by the caller)
-; cell top-left corner's x is at bp+6
-; cell top-left corner's y is at bp+8
+; Stack at start of useful work:
+;           y           BP+8 (cell's row number)
+;           x           BP+6 (cell's column number)
+;           RET ADDR    BP+4
+;           CX          BP+2 (caller's iteration counter)
+; SP -->    BP          BP+0
+; ----------------------------------------------------------------------------------------------------------------------
 FillCell:
     push cx
     push bp
@@ -72,9 +89,9 @@ FillCell:
     
     mov cx, CELL_SIZE-1
     Fill:
+        ; HLine(x+1, x+7, y+1)
         inc word [bp+8]
-        ; HLine(x+1, x+7, y)
-        push word [bp+8] ; y
+        push word [bp+8]
         mov ax, word [bp+6]
         add ax, 7
         push ax
@@ -89,22 +106,39 @@ FillCell:
     ret 4
 
 
-; cell's linear offset is at bp+4
-; DI <- x; SI <- y
+; ----------------------------------------------------------------------------------------------------------------------
+; Get the screen coordinates (x,y) of a maze cell's top-left corner from its offset in the maze array. This is done by
+; obtaining the cell coordinates from the equation cell_offset = cell_y*MAZE_COLS + cell_x and then scaling them down by
+; a factor of the square cell size.
+; Returns x in DI and y in SI.
+;
+; Stack at start of useful work:
+;           cell_offset BP+4 (cell's linear offset; can be any value in [0;MAZE_ROWS*MAZE_COLS])
+;           RET ADDR    BP+2
+; SP -->    BP          BP+0
+; ----------------------------------------------------------------------------------------------------------------------
 GetScreenCoords:
     push bp
     mov bp, sp
 
+    ; convert the linear offset to cell coordinates
+    ; cell_x := cell_offset % MAZE_COLS
+    ; cell_y := cell_offset / MAZE_COLS 
     mov ax, [bp+4]
     mov dl, MAZE_COLS
     div dl
 
+    ; DI <- cell_x
+    ; SI <- cell_y
     xor dx, dx
     mov dl, ah
     mov di, dx
     mov dl, al
     mov si, dx
 
+    ; convert cell coordinates to screen coordinates
+    ; DI <- DI*CELL_SIZE
+    ; SI <- SI*CELL_SIZE
     mov ax, di
     mov dl, CELL_SIZE
     imul dl
@@ -119,35 +153,64 @@ GetScreenCoords:
     ret 2
 
 
-; start cell is at bp+4
+; ----------------------------------------------------------------------------------------------------------------------
+; Explores the grid of cells using randomized Depth First Search. The function accepts the starting cell in the form of
+; its linear offset in the maze grid (that is, if the starting cell is identified by the couple (x,y), its linear offset
+; is y*40 + x, where y lies in [0;MAZE_ROWS] and x lies in [0;MAZE_COLS]). The starting cell is colored START_COLOR.
+;
+; Stack at start of useful work:
+;           start_cell  BP+4 (cell's linear offset; can be any value in [0;MAZE_ROWS*MAZE_COLS])
+;           RET ADDR    BP+2
+; SP -->    BP          BP+0
+; ----------------------------------------------------------------------------------------------------------------------
 DrawMaze:
     push bp
     mov bp, sp
 
+    ; set Visited array so border cells cant be walked on
     call VisitBorder
 
+    ; color-in the starting cell
     push word [bp+4]
     call GetScreenCoords
 
-    mov byte [Color], 0x9
+    mov byte [Color], START_COLOR
     push si ; y
     push di ; x
     call FillCell
 
-    mov byte [Color], 0xf
+    mov byte [Color], WALL_COLOR
     push word [bp+4]
     call Walk
+
+    ; recolor the starting cell as it's been covered up by the previous call
+    mov byte [Color], START_COLOR
+    push si ; y
+    push di ; x
+    call FillCell
 
     pop bp
     ret 2
 
 
-; linear index of current cell is at [bp+6]
+; ----------------------------------------------------------------------------------------------------------------------
+; Visits the cell given as parameter and discovers all paths that can be walked from it recursively. Each neighbor that
+; is visited from the parameter is colored DISCOVERED_COLOR and the  wall that separates them is colored VISITED_COLOR.
+; Dead-ends (cells whose neighbors have all been visited) are colored VISITED_COLOR. Delay has been implemented between 
+; visits to animate the search progression.
+;
+; Stack at start of useful work:
+;           visitee     BP+6 (cell's linear offset; can be any value in [0;MAZE_ROWS*MAZE_COLS])
+;           RET ADDR    BP+4
+;           CX          BP+2
+;           BP          BP+0
+; SP -->    neighbor_id BP-2
+; ----------------------------------------------------------------------------------------------------------------------
 Walk:
     push cx ; save caller's iteration number
     push bp
     mov bp, sp
-    sub sp, 2 ; space for next neighbor's index
+    sub sp, 2 ; make space for next neighbor's index
 
     ; visit the current cell
     mov bx, word [bp+6]
@@ -162,13 +225,13 @@ Walk:
     ; each cell waits a few microsecond before visiting its neighbors
     mov ah, 0x86
     xor cx, cx
-    mov dx, DELAY
+    mov dx, WALK_DELAY
     int 0x15
 
     mov cx, 4
 
     VisitNeighbors:
-        ; compute the linear offset of the neighbor
+        ; compute the linear offset of the neighbor's cell
         mov bx, word [bp-2]
         add bx, bx
         mov ax, word [bp+6]
@@ -176,7 +239,7 @@ Walk:
         mov bx, ax
 
         cmp byte [Visited + bx], 1
-        je Continue ; neighbor has been visited and must be skipped
+        je Continue ; neighbor has already been visited and must be skipped
 
         ; get the coordinates of the neighbor's cell top-left corner
         push bx
@@ -187,9 +250,8 @@ Walk:
         push di ; x
         call FillCell
 
-        mov byte [Color], DISCOVERED_COLOR
-
-        ; destroy the wall separating current cell and visited neighbor
+        mov byte [Color], VISITED_COLOR ; wall's color blends in with visited cells
+        ; destroy the wall separating current cell and the visited neighbor
         cmp word [bp-2], 0
         je DestroyRightWall     ; when visiting the western neighbor, destroy its eastern wall
         cmp word [bp-2], 1
@@ -200,7 +262,6 @@ Walk:
         je DestroyBottomWall    ; when visiting the northern neighbor, destory its southern wall
 
         ; cancel only the inner pixels so walls don't intersect
-        ; this is achieved by increasing the lower limit and decreasing the upper limit
         DestroyLeftWall:
             ; VLine(x, y+1, y+7)
             mov ax, si
@@ -253,7 +314,7 @@ Walk:
             call Walk
 
     Continue:
-        ; next <- (next + 1) % 4
+        ; next <- (next+1) % 4
         mov ax, word [bp-2]
         inc ax
         mov dl, 4
@@ -278,9 +339,16 @@ Walk:
         mov sp, bp
         pop bp
         pop cx ; restore caller's iteration number
-        ret 2 ; you gotta clear the parameter (word) from the stack!
+        ret 2
 
 
+; ----------------------------------------------------------------------------------------------------------------------
+; Marks the cells at the screen border as visited so the walking algorithm doesn't have to.
+;
+; Stack at start of useful work:
+;           RET ADDR    BP+2
+; SP -->    BP          BP+0
+; ----------------------------------------------------------------------------------------------------------------------
 VisitBorder:
     push bp
     mov bp, sp
@@ -307,9 +375,16 @@ VisitBorder:
     ret
 
 
-; min is at bp+4
-; max is at bp+6
-; generates a random number between min and max inclusive
+; ----------------------------------------------------------------------------------------------------------------------
+; Generate a random number on the interval [min;max] using a simple xor-shift generator.
+; Algorithm source: http://www.retroprogramming.com/2017/07/xorshift-pseudorandom-numbers-in-z80.html
+;
+; Stack at start of useful work:
+;           max         BP+6
+;           min         BP+4
+;           RET ADDR    BP+2
+; SP -->    BP          BP+0
+; ----------------------------------------------------------------------------------------------------------------------
 MiniRNG:
     push bp
     mov bp, sp
@@ -345,8 +420,14 @@ MiniRNG:
     ret 4
 
 
-; Color is set by the caller
-ClearScreen:
+; ----------------------------------------------------------------------------------------------------------------------
+; Floods the entire screen with a single color specified in the global variable Color.
+;
+; Stack at start of useful work:
+;           RET ADDR    BP+2
+; SP -->    BP          BP+0
+; ----------------------------------------------------------------------------------------------------------------------
+FillScreen:
     push bp
     mov bp, sp
 
@@ -354,20 +435,30 @@ ClearScreen:
     xor bx, bx
     mov al, [Color]
 
-    ClearScreenLoop:
+    FillScreenLoop:
         mov [es:bx], al
         inc bx
-        loop ClearScreenLoop
+        loop FillScreenLoop
 
     pop bp
     ret
 
 
-
-; x1 is at bp+14
-; x2 is at bp+16
-; y is at bp+18
-; Color is set by the caller
+; ----------------------------------------------------------------------------------------------------------------------
+; Draws a horizontal line connecting the ends x1 and x2 within the raster row y. Color is specified by the caller.
+;
+; Stack at start of useful work:
+;           y           BP+18 (raster row)
+;           x2          BP+16 (x coordinate of the right end of the line)
+;           x1          BP+14 (x coordinate of the left end of the line)
+;           RET ADDR    BP+12
+;           AX          BP+10
+;           BX          BP+8
+;           CX          BP+6
+;           DX          BP+4
+;           DI          BP+2
+; SP -->    BP          BP+0
+; ----------------------------------------------------------------------------------------------------------------------
 HLine:
     push ax
     push bx
@@ -377,6 +468,7 @@ HLine:
     push bp
     mov bp, sp
 
+    ; validate the coordinates
     ; x2 < SCREEN_WIDTH
     cmp word [bp+16], SCREEN_WIDTH
     jge EndHLine
@@ -393,13 +485,13 @@ HLine:
     cmp word [bp+18], SCREEN_HEIGHT
     jge EndHLine
 
-    ; line start at offset := 320*y + x1
+    ; line starts at offset 320*y + x1
     mov ax, SCREEN_WIDTH
     imul word [bp+18]
     add ax, word [bp+14]
     mov bx, ax
 
-    ; draw x2 - x1 + 1 pixels
+    ; set the next x2 - x1 + 1 pixels
     mov cx, word [bp+16]
     sub cx, word [bp+14]
     inc cx
@@ -421,11 +513,21 @@ HLine:
         ret 6
 
 
-
-; x is at bp+14
-; y1 is at bp+16
-; y2 is at bp+18
-; Color is set by the caller
+; ----------------------------------------------------------------------------------------------------------------------
+; Draws a vertical line connecting the ends y1 and y2 within the raster column x. Color is specified by the caller.
+;
+; Stack at start of useful work:
+;           y2          BP+18 (y coordinate of the higher end of the line)
+;           y1          BP+16 (y coordinate of the lower end of the line)
+;           x           BP+14 (raster column)
+;           RET ADDR    BP+12
+;           AX          BP+10
+;           BX          BP+8
+;           CX          BP+6
+;           DX          BP+4
+;           DI          BP+2
+; SP -->    BP          BP+0
+; ----------------------------------------------------------------------------------------------------------------------
 VLine:
     push ax
     push bx
@@ -435,6 +537,7 @@ VLine:
     push bp
     mov bp, sp
 
+    ; validate the coordinates
     ; y2 < SCREEN_HEIGHT
     cmp word [bp+18], SCREEN_HEIGHT
     jge EndVLine
@@ -451,13 +554,13 @@ VLine:
     cmp word [bp+14], SCREEN_WIDTH
     jge EndVLine
 
-    ; line start at offset := 320*y1 + x
+    ; line starts at offset 320*y1 + x
     mov ax, SCREEN_WIDTH
     imul word [bp+16]
     add ax, word [bp+14]
     mov bx, ax
 
-    ; draw y2 - y1 + 1 pixels
+    ; set the next y2 - y1 + 1 pixels
     mov cx, word [bp+18]
     sub cx, word [bp+16]
     inc cx
@@ -480,14 +583,24 @@ VLine:
 
 
 
-; Color is set by the caller
+; ----------------------------------------------------------------------------------------------------------------------
+; Draws a grid pattern of MAZE_COLS*MAZE_ROWS square cells of side CELL_SIZE representing the labyrinth's walls.
+; Walls (a cell's sides) are one-pixel-thick vertical and horizontal lines sharing the color specified in the WALL_COLOR
+; constant. Cells at the border of the screen are filled with the color specified in the BORDER_COLOR constant.
+;
+; Stack at start of useful work:
+;           RET ADDR    BP+2
+; SP -->    BP          BP+0
+; ----------------------------------------------------------------------------------------------------------------------
 DrawWalls:
     push bp
     mov bp, sp
 
+    mov byte [Color], WALL_COLOR
 
+    ; draw MAZE_ROWS rows of length SCREEN_WIDTH spaced CELL_SIZE pixels each
     xor dx, dx
-    mov cx, word MAZE_ROWS 
+    mov cx, word MAZE_ROWS
     RowLoop:
         push dx ; y
         push word SCREEN_WIDTH-1 ; x2
@@ -497,27 +610,32 @@ DrawWalls:
         add dx, CELL_SIZE
         loop RowLoop
 
-
+    ; draw MAZE_COLS columns of length SCREEN_HEIGHT spaced CELL_SIZE pixels each
     xor dx, dx
     mov cx, word MAZE_COLS
     ColLoop:
         push word SCREEN_HEIGHT-1 ; y2
         push word 0; y1
-        push dx; x
+        push dx ; x
         call VLine
 
         add dx, CELL_SIZE
         loop ColLoop
 
 
+    mov byte [Color], BORDER_COLOR
+
+    ; fill in the border cells with a single color by drawing 8 adjacent columns/rows for each side
     xor dx, dx
     mov cx, word CELL_SIZE
-    DrawBorder:
+    FillBorder:
+        ; top rows
         push dx ; y
         push SCREEN_WIDTH-1 ; x2
         push 0 ; x1
         call HLine
 
+        ; bottom rows
         mov ax, dx
         add ax, SCREEN_HEIGHT-CELL_SIZE
         push ax ; y
@@ -525,11 +643,13 @@ DrawWalls:
         push 0 ; x1
         call HLine
 
+        ; left columns
         push SCREEN_HEIGHT-1 ; y2
         push 0 ; y1
         push dx ; x
         call VLine
 
+        ; right columns
         mov ax, dx
         add ax, SCREEN_WIDTH-CELL_SIZE
         push SCREEN_HEIGHT-1
@@ -538,8 +658,7 @@ DrawWalls:
         call VLine
 
         inc dx
-        loop DrawBorder
-
+        loop FillBorder
 
     pop bp
     ret
